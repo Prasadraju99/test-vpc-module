68114817dca03d6ae662833e97b7a07a2df44b9f
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-10-24 21:48:28 +0530

    test-vpc-module

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.72.0"
+    }
+  }
+  backend "s3" {
+    bucket = "vpc-remote-state"
+    key    = "vpc-demo"
+    region = "us-east-1"
+    dynamodb_table = "vpc-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
