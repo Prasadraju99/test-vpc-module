6d524a23d5c30aa8c62c887f0de021f0d48ca897
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-10-31 19:04:09 +0530

    terraform-aws-vpc

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id   # calling output from main code
+  value = module.vpc.vpc_id # calling output from main code
 }
 
 # output "az_info" {
 #   value = module.vpc.az_info
 # }
+
+output "default_vpc_info" {
+  value = module.vpc.default_vpc_info
+}
+
+output "main_route_table_info" {
+  value = module.vpc.main_route_table_info
+}
